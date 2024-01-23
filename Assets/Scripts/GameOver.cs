dfc2cf251ba2444bea0c911984f620eccdcfa7dc
Rachel Gupta <rachelgupta24@gmail.com>
2024-01-23 07:00:32 +0530

    Game Over Page appears when the block falls

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag=="Player")
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+    public void RestartGane()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
